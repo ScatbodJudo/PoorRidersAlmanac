c5d38355db919c9a86cff7e7e9ca201e7e375534
Todd Jacobus <rtjacobus@alaska.edu>
2016-03-27 11:43:55 -0600

    trying to figure out how to manage assets for blog posts...added URL for AWS s3 images to pages#home

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,9 +1,18 @@
 class BlogController < ApplicationController
+
 	  def index
     @blogPosts = BlogPost.all #.order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
   	end
 
+  	def new
+  		BlogPost.title = "My New Post"
+  		BlogPost.subTitle = "A walk in the woods"
+  		BlogPost.date = "1/1/2016"
+  		BlogPost.authorName = "Todd Jacobus"
+  	end
 
 
 
 end
+
+new
